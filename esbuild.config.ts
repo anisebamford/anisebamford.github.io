6650d20d642c55c874f9d3d45d92070ab8d30859
Anise Bamford (they/she) <anisebamford@gmail.com>
2024-04-24 14:54:49 -0700

    Update esbuild.config.ts

--- esbuild.config.ts
+++ esbuild.config.ts
@@ -1,16 +1,20 @@
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 const isDev = process.env.NODE_ENV === "devx"
-
+const fs = require("fs");
+const {randomUUID} = require("crypto");
 const esbuild = require("esbuild");
+
 async function watch() {
+  const token = randomUUID();
   const options = ({
     entryPoints: ['src/app.tsx'],
     bundle: true,
     minify: isDev,
     format: 'cjs',
     sourcemap: isDev,
-    outfile: 'public/index.js',
+    outfile: `public/index-${token}.js`,
   });
+  fs.writeFileSync("public/index.html", ``)
     if (isDev) {
       const ctx = await esbuild.context(options)
       await ctx.watch();
