e4493ee1dd7474e24d82fff31744691dd45131cf
Anise Bamford (they/she) <anisebamford@gmail.com>
2024-11-01 11:28:03 -0800

    Improve readability

--- esbuild.config.ts
+++ esbuild.config.ts
@@ -3,9 +3,28 @@ const fs = require("fs");
 const {randomUUID} = require("crypto");
 const esbuild = require("esbuild");
 
+function writeIndexHtml(token: string, title: string) {
+  fs.writeFileSync("public/index.html", `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>${title}</title>
+  </head>
+  <body>
+  <div id="root"></div>
+	<script src="index-${token}.js"></script>
+  </body>
+</html>`);
+}
+
 async function build() {
   const isDev = process.env.NODE_ENV === "dev"
   const token = randomUUID();
+
+  writeIndexHtml(token, "Anise Bamford");
+	
   const options = ({
     entryPoints: ['src/app.tsx'],
     bundle: true,
@@ -14,26 +33,14 @@ async function build() {
     sourcemap: isDev,
     outfile: `public/index-${token}.js`,
   });
-  fs.writeFileSync("public/index.html", `<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Anise Bamford</title>
-  </head>
-  <body>
-  <div id="root"></div>
-	<script src="index-${token}.js"></script>
-  </body>
-</html>`)
-    if (isDev) {
-      const ctx = await esbuild.context(options)
-      await ctx.watch();
-      console.log('Watching...');
-    }
-    else {
-      esbuild.build(options)
-    }
+
+  if (isDev) {
+    const ctx = await esbuild.context(options)
+    console.log('Watching...');
+    await ctx.watch();
+  }
+  else {
+    esbuild.build(options)
+  }
 }
 build();
